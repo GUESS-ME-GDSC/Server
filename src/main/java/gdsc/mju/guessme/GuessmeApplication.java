ee7ca15c758194cb6e4bf92788dd6870d98b9d36
hou27 <ataj125@gmail.com>
2023-03-13 20:52:39 +0900

    project setting

---
+++ src/main/java/gdsc/mju/guessme/GuessmeApplication.java
@@ -0,0 +1,13 @@
+package gdsc.mju.guessme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdsc/mju/guessme/GuessmeApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.mju.guessme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
