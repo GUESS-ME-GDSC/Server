2158db561c8d929572896b0249d3ab43ff1045f8
hou27 <ataj125@gmail.com>
2023-03-19 22:18:21 +0900

    [Feat] #16 deleteInfoByIdList method 구현

---
+++ src/main/java/gdsc/mju/guessme/domain/info/dto/DeleteInfoByIdListReqDto.java
@@ -0,0 +1,17 @@
+package gdsc.mju.guessme.domain.info.dto;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DeleteInfoByIdListReqDto {
+    private List<Long> idList;
+
+    public DeleteInfoByIdListReqDto(List<Long> idList) {
+        System.out.println(idList);
+        this.idList = idList;
+    }
+
+}
--- src/main/java/gdsc/mju/guessme/domain/info/repository/InfoRepository.java
+++ src/main/java/gdsc/mju/guessme/domain/info/repository/InfoRepository.java
@@ -5,10 +5,18 @@ import gdsc.mju.guessme.domain.person.entity.Person;
 import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface InfoRepository extends JpaRepository<Info, Long> {
 
     List<Info> findAllByPerson(Person person);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Info e WHERE e.id IN :ids")
+    void deleteByIdIn(@Param("ids") List<Long> ids);
 
     @Transactional
     void deleteAllInBatchByPersonId(Long personId);
