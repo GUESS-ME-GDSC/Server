0a466d591b23c88a24474c68835e3414af612f89
hou27 <ataj125@gmail.com>
2023-03-26 22:11:31 +0900

    [Feat] #30 Spring Security 관련 코드 재구축
    deprecated된 feature를 걷어내고 개선하는 동시에
    https://github.com/hou27/spring-boot-jwt-example
    개인 샘플 코드를 참고하여 재구축

---
+++ src/main/java/gdsc/mju/guessme/global/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package gdsc.mju.guessme.global.config;
+
+import gdsc.mju.guessme.domain.auth.jwt.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import lombok.RequiredArgsConstructor;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    // 추가된 jwt 관련 친구들을 security config에 추가
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Disable csrf to use token
+        http
+            .csrf().disable();
+
+        //
+        http
+            .authorizeRequests()
+            .antMatchers(
+                "/",
+                "/auth/signUp",
+                "/user/userList",
+                "/auth/signIn*",
+                "/user/profile/view/**",
+                "/auth/regenerateToken",
+                "/favicon.ico"
+            ).permitAll()
+            .anyRequest().authenticated();
+
+        // No session will be created or used by spring security
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // Apply JWT
+        http.apply(new JwtSecurityConfig(jwtTokenProvider));
+
+        return http.build();
+    }
+}
