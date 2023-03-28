421567ca996939865c8d4feb2039692cac280563
bluesun147 <bluesun147@naver.com>
2023-03-28 22:22:43 +0900

    [Feat] 텍스트 추출 원격 이미지에서 하는 방식으로 변경

--- src/main/java/gdsc/mju/guessme/domain/quiz/dto/ScoreReqDto.java
+++ src/main/java/gdsc/mju/guessme/domain/quiz/dto/ScoreReqDto.java
@@ -3,12 +3,15 @@ package gdsc.mju.guessme.domain.quiz.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class ScoreReqDto {
     private String infoValue;
     private String answer; // 사진
-    private String filePath; // 이미지 파일 경로
+    private MultipartFile image;
 }
