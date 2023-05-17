c4d253761d375627a5a969eeb601b1ceb12b71d1
hou27 <ataj125@gmail.com>
2023-05-18 01:56:02 +0900

    [Feat] #78 existsByQuestionAndPerson method 제거
    refactoring 과정에서 사용하지 않게 된 메서드 제거

--- src/main/java/gdsc/mju/guessme/domain/quiz/repository/ScoringRepository.java
+++ src/main/java/gdsc/mju/guessme/domain/quiz/repository/ScoringRepository.java
@@ -5,7 +5,6 @@ import gdsc.mju.guessme.domain.quiz.entity.Scoring;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScoringRepository extends JpaRepository<Scoring, Long> {
-    boolean existsByQuestionAndPerson(String question, Person person);
     Scoring findByQuestionAndPerson(String question, Person person);
     void deleteByQuestionAndPerson(String question, Person person);
 }
