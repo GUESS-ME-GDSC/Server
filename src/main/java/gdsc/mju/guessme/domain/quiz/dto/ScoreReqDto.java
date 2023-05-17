8369014c2a166a3bab898bc32291f1b5fb668dd6
bluesun147 <bluesun147@naver.com>
2023-05-17 23:33:19 +0900

    [Feat] #72 채점 방식 변경으로 채점 request dto 수정

--- src/main/java/gdsc/mju/guessme/domain/quiz/dto/ScoreReqDto.java
+++ src/main/java/gdsc/mju/guessme/domain/quiz/dto/ScoreReqDto.java
@@ -11,6 +11,9 @@ import org.springframework.web.multipart.MultipartFile;
 @NoArgsConstructor
 @ToString
 public class ScoreReqDto {
+    private Long infoId;
+    private String infoKey;
     private String infoValue;
+    private Long personId;
     private MultipartFile image;
 }
