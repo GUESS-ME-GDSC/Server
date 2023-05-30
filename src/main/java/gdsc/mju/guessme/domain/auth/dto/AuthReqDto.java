5b481ab210c72d2a866346c5300fc1fb400d4f7c
bluesun147 <bluesun147@naver.com>
2023-05-30 21:35:41 +0900

    [fix] #88 스프링 파일 업로드 용량제한 설정

--- src/main/java/gdsc/mju/guessme/domain/auth/dto/AuthReqDto.java
+++ src/main/java/gdsc/mju/guessme/domain/auth/dto/AuthReqDto.java
@@ -2,10 +2,12 @@ package gdsc.mju.guessme.domain.auth.dto;
 
 import gdsc.mju.guessme.domain.user.entity.User;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
+@NoArgsConstructor
 public class AuthReqDto {
     private String userId;
     private String userPassword;
@@ -19,9 +21,9 @@ public class AuthReqDto {
 
     public User toUserEntity() {
         return User.builder()
-            .userId(userId)
-            .userPassword(userPassword)
-            .email(email)
-            .build();
+                .userId(userId)
+                .userPassword(userPassword)
+                .email(email)
+                .build();
     }
 }
